84f93c45d7e278aef23adec09eb3a314172b2ad1
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-06-22 15:23:18 +0100

    Title component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Title from "./components/Title";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Title />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Title() {
+  return (
+    <header>
+      <h1>Avalanche Reporter</h1>
+    </header>
+  );
+}

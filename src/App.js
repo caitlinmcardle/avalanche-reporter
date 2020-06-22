3ccdedfca1c72ea2dd16ad63a8c0e4caa717944f
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-06-22 15:34:23 +0100

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
+import { Router } from "@reach/router";
 import Title from "./components/Title";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "@reach/router";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Forecasts</li>
+        <li>Report an Avalanche</li>
+      </ul>
+    </nav>
+  );
+}

157d8d31b614fa1d69fc571480efc6fe45232bd0
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-06-22 15:54:04 +0100

    area initialised and router set up

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import "./App.css";
 import { Router } from "@reach/router";
 import Title from "./components/Title";
 import NavBar from "./components/NavBar";
+import Area from "./components/Area";
 
 function App() {
   return (
     <div className="App">
       <Title />
       <NavBar />
+      <Router>
+        <Area path="/" />
+      </Router>
     </div>
   );
 }
---
+++ src/components/Area.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Area extends Component {
+  state = {
+    areas: [],
+    selectedArea: "",
+  };
+  render() {
+    return (
+      <main>
+        <p>Areas here</p>
+      </main>
+    );
+  }
+}

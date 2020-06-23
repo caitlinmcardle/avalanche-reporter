072ac4ac1249651e1e316286c43dcac2f1fc4b64
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-06-23 11:02:30 +0100

    Individual report api request

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+import Loader from "./Loader";
 
-export default function Report() {
-  return (
-    <main>
-      <p>report here</p>
-    </main>
-  );
+export default class Report extends Component {
+  state = {
+    report: {},
+    isLoading: true,
+  };
+
+  componentDidMount() {
+    this.getReport();
+  }
+
+  getReport = () => {
+    console.log(this.props);
+    axios
+      .get(
+        `https://cmc-final-project.herokuapp.com/avalanche-reports/${this.props.report_id}`
+      )
+      .then(({ data }) => {
+        this.setState({ report: data, isLoading: false });
+      });
+  };
+  render() {
+    if (this.state.isLoading) return <Loader />;
+    const {
+      Date,
+      Time,
+      Age,
+      Size,
+      Elevation,
+      Trigger,
+      Type,
+      area,
+      Aspect,
+    } = this.state.report;
+    return (
+      <main>
+        <h2>Date: {Date}</h2>
+        <h3>Area: {area.Name}</h3>
+        <p>Time: {Time}</p>
+        <p>Size: {Size}</p>
+        <p>Age: {Age} hr(s)</p>
+        <p>Elevation: {Elevation} m</p>
+        <p>Trigger: {Trigger}</p>
+        <p>Type: {Type}</p>
+        <p>Aspect: {Aspect}</p>
+      </main>
+    );
+  }
 }

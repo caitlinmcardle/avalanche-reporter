14ae442168c52cd48cec78bb336a10ad03356792
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-06-23 10:27:02 +0100

    getReports api call

--- src/components/ReportList.jsx
+++ src/components/ReportList.jsx
@@ -1,10 +1,41 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Loader from "./Loader";
 
 export default class ReportList extends Component {
+  state = {
+    reports: [],
+    isLoading: true,
+  };
+
+  componentDidMount() {
+    this.getReports();
+  }
+
+  getReports = () => {
+    axios
+      .get("https://cmc-final-project.herokuapp.com/avalanche-reports")
+      .then(({ data }) => {
+        this.setState({ reports: data, isLoading: false }, () => {
+          console.log(this.state);
+        });
+      });
+  };
   render() {
+    if (this.state.isLoading) return <Loader />;
+    const { reports } = this.state;
     return (
       <main>
-        <p>ReportList here</p>
+        <h2>All Avalanche Reports</h2>
+        {reports.map((report) => {
+          return (
+            <article key={report.id}>
+              <h4>Date: {report.Date}</h4>
+              <p>Area: {report.area.Name}</p>
+              <p>Size: {report.Size}</p>
+            </article>
+          );
+        })}
       </main>
     );
   }

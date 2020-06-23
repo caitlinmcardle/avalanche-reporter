64860469460a5c847900c855c2ab58dde9fc661e
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-06-23 12:33:09 +0100

    Added link to individual report page

--- src/components/ReportCard.jsx
+++ src/components/ReportCard.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 export default function ReportCard({ id, Date, area, Size }) {
   return (
     <article key={id}>
-      <h4>Date: {Date}</h4>
+      <Link to={`/reports/${id}`}>
+        <h4>Date: {Date}</h4>
+      </Link>
       <p>Area: {area.Name}</p>
       <p>Size: {Size}</p>
     </article>

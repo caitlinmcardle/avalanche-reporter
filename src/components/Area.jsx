156dd7676b98f848a21e8324ac1f7d1efaca81c2
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-06-23 16:09:47 +0100

    Can get reports for an area

--- src/components/Area.jsx
+++ src/components/Area.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Loader from "./Loader";
+import ReportCard from "./ReportCard";
 
 export default class Area extends Component {
   state = {
     areas: [],
-    selectedArea: "",
+    selectedAreaId: "1",
     isLoading: true,
+    selectedReports: [],
   };
 
   componentDidMount() {
@@ -23,14 +25,48 @@ export default class Area extends Component {
       );
   };
 
+  getAreaReports = () => {
+    axios
+      .get(
+        `https://cmc-final-project.herokuapp.com/avalanche-reports/?area.id=${this.state.selectedAreaId}`
+      )
+      .then(({ data }) =>
+        this.setState(
+          (currentState) => {
+            return { selectedReports: data };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  };
+
+  handleSubmit = (submitEvent) => {
+    submitEvent.preventDefault();
+    this.getAreaReports();
+  };
+
+  handleInput = (inputEvent) => {
+    const areaId = inputEvent.target.value;
+    this.setState(
+      (currentState) => {
+        return { selectedAreaId: areaId };
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+
   render() {
     if (this.state.isLoading) return <Loader />;
-    const { areas } = this.state;
+    const { areas, selectedReports } = this.state;
     return (
       <main className="main">
-        <form>
+        <form className="form" onSubmit={this.handleSubmit}>
           <p>Choose an area:</p>
-          <select>
+          <select name="area" onChange={this.handleInput}>
             {areas.map((area) => {
               return (
                 <option key={area.id} value={area.id}>
@@ -41,6 +77,9 @@ export default class Area extends Component {
           </select>
           <button>Go</button>
         </form>
+        {selectedReports.map((report) => {
+          return <ReportCard key={report.id} {...report} />;
+        })}
       </main>
     );
   }

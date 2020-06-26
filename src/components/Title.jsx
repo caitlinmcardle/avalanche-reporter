06efa4f21eb26f5e4c1828d0525c05eaba2aae6d
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-06-26 14:56:55 +0100

    Added classname to title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Title() {
   return (
     <header>
-      <h1>Avalanche Reporter</h1>
+      <h1 className="title">Avalanche Reporter</h1>
     </header>
   );
 }

28f3115e001398c8a43589c2fcc586888561f96f
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-06-26 09:58:12 +0100

    Made map full width of screen

--- src/components/AreaMap.jsx
+++ src/components/AreaMap.jsx
@@ -9,7 +9,7 @@ export default function AreaMap({
 }) {
   return (
     <section>
-      <Map center={[51.3, -118.5]} zoom={5}>
+      <Map center={[51.3, -118.5]} zoom={6}>
         {selectedReports.map((report) => (
           <Marker
             key={report.id}
